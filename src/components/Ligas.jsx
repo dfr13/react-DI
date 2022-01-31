0a6189750cb50a91a513f6f4650d5b092fd499f2
dfr13 <danielferrerroman0@gmail.com>
2022-01-31 14:05:01 +0100

    Queda la foto

---
+++ src/components/Ligas.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import { Card, Container, Table, Row, Col } from 'react-bootstrap';
+import FilmCard from '../components/FilmCard';
+import uuid from 'react-uuid';
+
+class Ligas extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedItem: '',
+      tableData: [],
+    };
+  }
+  async componentDidMount() {
+    const response = await fetch(
+      'https://api-football-standings.azharimm.site/leagues'
+    );
+    const responseData = await response.json();
+    this.setState({
+      tableData: responseData.data,
+      selectedItem: responseData.data[0],
+    });
+  }
+
+  changeClicked(item) {
+    this.setState({
+      selectedItem: item,
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Col lg={8} md={6}>
+              <h1>Ligas Profesionales</h1>
+              <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>Nombre</th>
+                    <th>Liga</th>
+                    <th>Abreviatura</th>
+                    <th>logos</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.state.tableData.map((item) => {
+                    return (
+                      <tr key={uuid()} onClick={() => this.changeClicked(item)}>
+                        <td>{item.name}</td>
+                        <td>{item.slug}</td>
+                        <td>{item.abbr}</td>
+                        <td>{item.logos.dark}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </Table>
+            </Col>
+            <Col lg={4} md={6}>
+              {/*<FilmCard data = {this.state.selectedItem}>*/}
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={this.state.selectedItem.logos.light} />
+                <Card.Body>
+                  <Card.Title>
+                    Titulo Original: {this.state.selectedItem.name}
+                    <p />
+                    Abreviatura: {this.state.selectedItem.abbr}
+                  </Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+export default Ligas;
--- src/data/MenuItems.js
+++ src/data/MenuItems.js
@@ -2,11 +2,11 @@ import  Home  from '../components/Home';
 import { PaginaListas } from '../components/PaginaListas';
 import Perfil  from '../components/Perfil';
 import  BoostrapTablaClase  from '../components/BoostrapTablaClase';
-import PelisGhibli from '../components/PelisGhibli';
+import Ligas from '../components/Ligas';
 export const MenuItems = [
 {path:'/' ,id:1 ,title:'Home', components:Home},
 {path:'/listas' ,id:2 ,title:'Listas de tareas', components:PaginaListas},
 {path:'/perfil' ,id:3 ,title:'Perfil', components:Perfil},
 {path:'/boostraptablaclase' ,id:4 ,title:'Equipos', components:BoostrapTablaClase},
-{path:'/pelisgihbili' ,id:5 ,title:'Peliculas', components:PelisGhibli}
+{path:'/ligas' ,id:5 ,title:'Ligas', components:Ligas}
 ];
